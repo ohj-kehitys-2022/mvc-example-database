7ad6af636f1a18da5cf862a76220befe4c902b98
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2022-10-27 15:35:28 +0300

    login toimii

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/book');
 var borrowersRouter = require('./routes/borrower');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -19,8 +19,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/book', booksRouter);
 app.use('/borrower', borrowersRouter);
+app.use('/login', loginRouter);
 
 module.exports = app;
