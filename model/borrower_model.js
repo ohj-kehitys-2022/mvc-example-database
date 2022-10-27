df07bfdd55244640622ce1d2366123ea52778ff4
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2022-10-27 14:46:56 +0300

    salasanojen kryptaus toimii

--- model/borrower_model.js
+++ model/borrower_model.js
@@ -1,22 +1,29 @@
-const db=require('../database');
+const db = require('../database');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
-const borrower={
-    getAllBorrowers: function(callback){
-        return db.query('select * from borrower',callback);
+const borrower = {
+    getAllBorrowers: function (callback) {
+        return db.query('select * from borrower', callback);
     },
-    getOneBorrower: function(id,callback){
-        return db.query('select * from borrower where id_borrower=?',[id],callback);
+    getOneBorrower: function (id, callback) {
+        return db.query('select * from borrower where id_borrower=?', [id], callback);
     },
-    addBorrower: function(insertData,callback){
-        return db.query('insert into borrower values(?,?,?,?)',
-        [insertData.id_borrower, insertData.fname, insertData.lname, insertData.password],callback);
+    addBorrower: function (insertData, callback) {
+        bcrypt.hash(insertData.password, saltRounds, function (err, hashedPassword) {
+            return db.query('insert into borrower values(?,?,?,?)',
+                [insertData.id_borrower, insertData.fname, insertData.lname, hashedPassword], callback);
+        });
     },
-    updateBorrower:function(id,updateData,callback){
-        return db.query('update borrower set fname=?, lname=?, password=? where id_borrower=?',[updateData.fname, updateData.lname, updateData.password, id],callback);
+    updateBorrower: function (id, updateData, callback) {
+        bcrypt.hash(updateData.password, saltRounds, function (err, hashedPassword) {
+            return db.query('update borrower set fname=?, lname=?, password=? where id_borrower=?', [updateData.fname, updateData.lname, hashedPassword, id], callback);
+        })
+
     },
-    deleteBorrower:function(id,callback){
-        return db.query('delete from borrower where id_borrower=?',[id],callback);
+    deleteBorrower: function (id, callback) {
+        return db.query('delete from borrower where id_borrower=?', [id], callback);
     }
 }
 
-module.exports=borrower;
+module.exports = borrower;
